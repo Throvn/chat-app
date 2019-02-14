b2c47e31e7e555a2efe9cfe545ec294288310776
throvn <Faceformer1>
2019-02-14 14:47:43 +0100

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const publicPath = path.join(__dirname, '/../public/');
+const port = process.env.PORT || 3000;
+console.log(publicPath);
+
+app.use(publicPath);
+
+app.get('/', (res, req) => {
+    console.log('hallo');
+    
+    req.sendFile(publicPath);
+})
+
+app.listen(3000, () => { console.log("Server is up on port" + port); })
