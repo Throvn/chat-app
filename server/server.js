6eb744caa5e4647856adbbaa2c4f30efa99ce42f
throvn <Faceformer1>
2019-02-15 19:47:34 +0100

    Create genereateMessage util

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const path = require('path');
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
+
+const {generateMessage} = require('./utils/message.js');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server)
@@ -19,6 +22,15 @@ io.on('connection', (socket) => {
         console.log('User disconnected');
         
     })
-})
+
+    socket.on('createMessage', (msg) => {
+        console.log(msg)
+        io.emit('newMessage', generateMessage(msg.from, msg.text))
+        
+    })
+
+    socket.emit('newMessage', generateMessage("Admin", "You joined the chatroom"));
+    socket.broadcast.emit('newMessage', generateMessage("Admin", "New User joined"))
+});
 
 server.listen(3000, () => { console.log("Server is up on port" + port); })
